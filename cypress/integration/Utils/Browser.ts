bbad75481e81724dc3cce0e0e336f568820192c7
Doug Bowen <douglastylerbowen@gmail.com>
2021-10-06 13:43:53 -0500

    push all

---
+++ cypress/integration/Utils/Browser.ts
@@ -0,0 +1,20 @@
+export class Browser {
+    _baseUrl: string = 'http://localhost:3000/';
+    _headed: boolean = false;
+    Directions = {
+        forward: 1,
+        backward: -1
+    };
+
+    launch(passedUrl: string = this._baseUrl){
+        let options = {
+            url: passedUrl,
+            timeout: 30000
+        }
+        return cy.visit(options);
+    }
+
+    delay(ms: number) {
+        return new Promise( resolve => setTimeout(resolve, ms) );
+    }
+}
